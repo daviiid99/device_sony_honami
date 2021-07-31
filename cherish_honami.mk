0a55d5bdd2fbddcd01b8985aaba4e670837bc52b
David <davidmolinamorales1999@gmail.com>
2021-07-31 14:43:02 +0200

    my fault

--- cherish_honami.mk
+++ cherish_honami.mk
@@ -8,7 +8,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.1.1 14.6.A.1.23
 
 BUILD_FINGERPRINT := Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release-keys
 
-PRODUCT_NAME := lineage_honami
+PRODUCT_NAME := cherish_honami
 PRODUCT_DEVICE := honami
 
 # Boot animation
