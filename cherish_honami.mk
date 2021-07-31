9d7f1e978e899090c6ebd2d4c5b0498fa0dbe6f2
David <davidmolinamorales1999@gmail.com>
2021-07-31 12:26:29 +0200

    Rename paths

--- cherish_honami.mk
+++ cherish_honami.mk
@@ -1,5 +1,5 @@
 # Inherit lineage common Phone stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 $(call inherit-product, device/sony/honami/full_honami.mk)
 
