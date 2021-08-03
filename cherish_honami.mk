7f7084ea6b72ec1d6e25afa3baa0f0ffd4dd45c8
David <davidmolinamorales1999@gmail.com>
2021-08-03 05:23:38 +0200

    WIP

--- cherish_honami.mk
+++ cherish_honami.mk
@@ -3,14 +3,25 @@ $(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 $(call inherit-product, device/sony/honami/full_honami.mk)
 
+# Inherit some common cherish stuff.
+WITH_GMS := true
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := armv7a
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_PIXEL_CHARGER := true
+CHERISH_BUILD_TYPE := OFFICIAL
+
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.1.1 14.6.A.1.236 2031203603 release-keys"
 
-BUILD_FINGERPRINT := Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release-keys
-
+# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := cherish_honami
 PRODUCT_DEVICE := honami
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := Honami
+PRODUCT_MANUFACTURER := Sony
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
+
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.cherish.maintainer=daviiid99
