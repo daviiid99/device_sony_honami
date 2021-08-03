ea9f345b300bec8ea086ab3c5ce2355aa12d0c41
David <davidmolinamorales1999@gmail.com>
2021-08-03 13:31:38 +0200

    Update cherish_honami.mk

--- cherish_honami.mk
+++ cherish_honami.mk
@@ -4,10 +4,8 @@ $(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 $(call inherit-product, device/sony/honami/full_honami.mk)
 
 # Inherit some common cherish stuff.
-WITH_GMS := true
 $(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_PIXEL_CHARGER := true
 CHERISH_BUILD_TYPE := OFFICIAL
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903
@@ -20,7 +18,7 @@ PRODUCT_BRAND := Sony
 PRODUCT_MODEL := Honami
 PRODUCT_MANUFACTURER := Sony
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
+BUILD_FINGERPRINT := Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release-keys
 
 PRODUCT_GENERIC_PROPERTIES += \
     ro.cherish.maintainer=daviiid99
